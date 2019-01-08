875ba650515517e9ee28b3830f0275337ed99745
Xenossolitarius <ipesic@fesb.hr>
2019-01-08 10:59:54 +0100

    Fetching colors & storing to state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,25 @@ import './component/ColorChanger'
 import ColorChanger from './component/ColorChanger';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+      this.state ={colors:['000000']}
+  }
+
+  componentDidMount(){
+   Promise.all([
+    fetch('http://www.colr.org/json/color/random'),
+    fetch('http://www.colr.org/json/color/random')
+   ]).then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
+   .then(([data1, data2]) => this.setState( prevState => ({
+     colors: [...prevState.colors, data1.colors[0].hex, data2.colors[0].hex]
+   })));
+       
+   
+}
+
+
+
   render() {
     return (
       <div className="App">
